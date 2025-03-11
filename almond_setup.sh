0081450eaff670fc11dbe845231c1d01fd011dd4
Shawn Patel <shawnptl8@gmail.com>
2025-03-11 09:18:05 -0700

    update setup file

--- almond_setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python3 almond_setup.py build_ext --inplace
---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+INSTALL_PATH=$1
+
+python3 setup.py build_ext --inplace
+
+if [ -n "$INSTALL_PATH" ]; then
+    BUILD_ARTIFACT=$(find . -name "*.so" | head -n 1)
+    mv "$BUILD_ARTIFACT" "$INSTALL_PATH"
+fi
+
